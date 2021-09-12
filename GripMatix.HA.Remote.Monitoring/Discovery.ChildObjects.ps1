f299627b39ea6d1360b6d052b9b586eef53d95b5
Merijn Overgaauw @GripMatix <76749035+merijnovergaauw@users.noreply.github.com>
2021-09-12 14:57:27 +0200

    .

---
+++ GripMatix.HA.Remote.Monitoring/Discovery.ChildObjects.ps1
@@ -0,0 +1,73 @@
+﻿## This script discovers Child Objects which are going to be managed by the Resource Pools that manage their Root Objects.
+## Author: Merijn Overgaauw (GripMatix)
+
+param($sourceId, $managedEntityId)
+$params = "SourceId: $sourceId, ManagedEntityId: $managedEntityId"
+
+# Constants
+$debug = $true
+$objectsToManagePath = "c:\SCOM\ChildObjects.csv"
+
+# Constants used for event logging
+$SCRIPT_NAME			= 'Discovery.ChildObjects.ps1'
+$EVENT_LEVEL_ERROR 		= 1
+$EVENT_LEVEL_WARNING 	= 2
+$EVENT_LEVEL_INFO 		= 4
+
+$SCRIPT_STARTED			= 1111
+$PROPERTYBAG_CREATED	= 1112
+$ERROR_GENERATED        = 1113
+$INFO_GENERATED         = 1114
+$SCRIPT_ENDED			= 1115
+
+#==================================================================================
+# Sub:		LogDebugEvent
+# Purpose:	Logs an informational event to the Operations Manager event log
+#			only if Debug argument is true
+#==================================================================================
+function Log-DebugEvent
+	{
+		param($eventNo, $eventLevel, $message)
+
+		$message = "`n" + $message
+		if ($debug)
+		{
+			$api.LogScriptEvent($SCRIPT_NAME,$eventNo,$eventLevel,$message)
+		}
+	}
+
+$api = New-Object -comObject 'MOM.ScriptAPI'
+$discoveryData = $api.CreateDiscoveryData(0, $SourceId, $ManagedEntityId)
+
+$message = "$SCRIPT_NAME script started.`nParameters:`n$params"
+Log-DebugEvent $SCRIPT_STARTED $EVENT_LEVEL_INFO $message
+
+$childObjects = Import-Csv "$objectsToManagePath" -Delimiter ","
+
+foreach ($childObject in $childObjects)
+	{
+	$rootObjectName = $childObject.RootObjectName
+	$rootObjectDeviceKey = $childObject.RootObjectDeviceKey
+	$childObjectName = $childObject.ChildObjectName
+	$childObjectTarget = $childObject.ChildObjectTarget
+	$childObjectMeta = $childObject.ChildObjectDescription
+
+	$Instance = $discoveryData.CreateClassInstance("$MPElement[Name='GripMatix.HA.Remote.Monitoring.RootObject']$")
+	$Instance.AddProperty("$MPElement[Name='SNL!System.NetworkManagement.Node']/DeviceKey$", "$rootObjectDeviceKey")
+	$Instance.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", "$rootObjectName")
+	$discoveryData.AddInstance($Instance)
+
+	$Instance = $discoveryData.CreateClassInstance("$MPElement[Name='GripMatix.HA.Remote.Monitoring.ChildObject']$")
+	$Instance.AddProperty("$MPElement[Name='SNL!System.NetworkManagement.Node']/DeviceKey$", $rootObjectDeviceKey)
+	$Instance.AddProperty("$MPElement[Name='GripMatix.HA.Remote.Monitoring.ChildObject']/Name$", $childObjectName)
+	$Instance.AddProperty("$MPElement[Name='GripMatix.HA.Remote.Monitoring.ChildObject']/Target$", $childObjectTarget)
+	$Instance.AddProperty("$MPElement[Name='GripMatix.HA.Remote.Monitoring.ChildObject']/Meta$", $childObjectMeta)
+	$Instance.AddProperty("$MPElement[Name='GripMatix.HA.Remote.Monitoring.ChildObject']/RootObjectName$", $rootObjectName)
+	$Instance.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", $childObjectName)
+	$discoveryData.AddInstance($Instance)
+	}	
+
+$discoveryData
+
+$message = "$SCRIPT_NAME script ended.`nParameters:`n$params"
+Log-DebugEvent $SCRIPT_ENDED $EVENT_LEVEL_INFO $message
---
+++ GripMatix.HA.Remote.Monitoring/Monitor.Check1.ps1
@@ -0,0 +1,57 @@
+﻿## This script monitors something.
+## Author: Merijn Overgaauw (GripMatix)
+
+param($monitorTarget)
+$params = "MonitorTarget: $monitorTarget"
+
+# Constants
+$debug = $true
+
+# Constants used for event logging
+$SCRIPT_NAME			= 'Monitor.Check1'
+$EVENT_LEVEL_ERROR 		= 1
+$EVENT_LEVEL_WARNING 	= 2
+$EVENT_LEVEL_INFO 		= 4
+
+$SCRIPT_STARTED			= 3111
+$PROPERTYBAG_CREATED	= 3112
+$ERROR_GENERATED        = 3113
+$INFO_GENERATED         = 3114
+$SCRIPT_ENDED			= 3115
+
+#==================================================================================
+function Log-DebugEvent
+	{
+		param($eventNo, $eventLevel, $message)
+
+		$message = "`n" + $message
+		if ($debug)
+		{
+			$api.LogScriptEvent($SCRIPT_NAME,$eventNo,$eventLevel,$message)
+		}
+	}
+#==================================================================================
+
+# Start script by setting up API object
+$api = New-Object -comObject 'MOM.ScriptAPI'
+
+$message = "$SCRIPT_NAME script started. Parameters:`n$params"
+Log-DebugEvent $SCRIPT_STARTED $EVENT_LEVEL_INFO $message
+
+$random = Get-Random -Minimum 1 -Maximum 100
+
+$cpuPct = Invoke-Command -ComputerName $monitorTarget -ScriptBlock {
+	Get-Ciminstance Win32_Processor | ? {$_.LoadPercentage -ne $null} | Measure-Object -Property LoadPercentage -Average | % {$_.Average}
+	}
+
+	
+$bag = $api.CreatePropertyBag()
+$bag.AddValue('CpuPct',$cpuPct)
+
+$bag
+
+$message = "CpuPct: $cpuPct"
+Log-DebugEvent $PROPERTYBAG_CREATED $EVENT_LEVEL_INFO $message
+
+$message = "$SCRIPT_NAME script ended. Parameters:`n$params"
+Log-DebugEvent $SCRIPT_ENDED $EVENT_LEVEL_INFO $message
